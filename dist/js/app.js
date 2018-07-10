d9f789d8a2e7e962a2a00ba6529fc865f191fee2
Dmytro Kutsaniuk <kutsaniuk@gmail.com>
2018-07-10 23:20:50 +0300

    Add projects page

--- dist/js/app.js
+++ dist/js/app.js
@@ -38,14 +38,18 @@ function showBalance(acc){
         nextArrow: $('.next'),
     });
 
+    animate();
+
     $(document).on("mousewheel", function() {
+        animate();
+    });
+
+    function animate() {
         var structure = $('#structure');
-        var offsetScrollStructure = $(document).scrollTop();
+        var offset = $(document).scrollTop();
         var offsetStructure = structure[0].offsetTop * 0.7;
 
-        console.log(offsetScrollStructure, offsetStructure);
-
-        if (offsetScrollStructure > offsetStructure) {
+        if (offset > offsetStructure) {
             structure.find('.title').addClass('show');
             structure.find('.description').addClass('show');
         } else {
@@ -53,11 +57,32 @@ function showBalance(acc){
             structure.find('.description').removeClass('show');
         }
 
-        if (offsetScrollStructure > offsetStructure * 1.4) {
+        if (offset > offsetStructure * 1.4) {
             structure.find('img').addClass('show');
         } else {
             structure.find('img').removeClass('show');
         }
-    });
+
+        var projects = $('#projects');
+        var offsetProjects = projects[0].offsetTop * 0.8;
+
+        if (offset > offsetProjects) {
+            projects.find('.title').addClass('show');
+        } else {
+            projects.find('.title').removeClass('show');
+        }
+
+        if (offset > offsetProjects * 1.1) {
+            projects.find('.row:first-child .col-md-6').addClass('show');
+        } else {
+            projects.find('.row:first-child .col-md-6').removeClass('show');
+        }
+
+        if (offset > offsetProjects * 1.25) {
+            projects.find('.row:last-child .col-md-6').addClass('show');
+        } else {
+            projects.find('.row:last-child .col-md-6').removeClass('show');
+        }
+    }
 });
 
